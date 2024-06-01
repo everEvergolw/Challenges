3c3f0ccf64954e334095f8c8818110ef6fc2baf0
everEvergolw <1092547508@qq.com>
2024-06-01 14:29:11 +0100

    design:Navbar and Footer

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+const Navbar = () =>{
+
+    return (
+        
+        <div className="bg-white  shadow ">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
+            <div className="flex justify-between items-center">
+            
+            <p className="font-bold text-2xl">
+
+                code challenges
+
+            </p>
+
+            <div className="">
+                <button className="bg-indigo-500 rounded px-3 py-1.5 my-4">
+                    Logout
+                </button>
+
+
+            </div>
+        
+           
+            </div>
+        </div>
+        </div>
+
+    )
+
+
+
+}
+
+export default Navbar
