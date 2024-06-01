6e5417eed5bef90786b25e9dc1ddfece97ae90cf
everEvergolw <1092547508@qq.com>
2024-06-01 15:22:39 +0100

    design:Featrues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Features from "./components/Features"
 
 function App() {
 
@@ -15,11 +16,16 @@ function App() {
     
     */}
     
-      <Navbar/>
-      <Footer/>
+        <Navbar/>
+        <Features/>
+
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+
+
+      <Footer/>
+
     </>
   )
 }
